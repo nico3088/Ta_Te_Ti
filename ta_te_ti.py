c475f6e33a567fa658b753257b5f984fa42e1a07
Nicolas Garnero <nicogarnero1@outlook.com>
2023-03-13 12:51:31 -0300

    :subiendo progreso

--- ta_te_ti.py
+++ ta_te_ti.py
@@ -14,4 +14,12 @@ def imprimir_tablero(tablero):
             else:
                 print(fila[i], end="  ")
 
-imprimir_tablero(ta)
+turno_1 = True
+jugador_1 = " "
+jugador_2 = " "
+turno = 0
+
+imprimir_tablero(tablero)
+while turno < 9:
+    if jugador_1 == " ":
+                         
