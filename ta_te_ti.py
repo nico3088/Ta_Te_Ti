3ef3ff682e0cc42e2829edb472d97cfd586b9386
Nicolas Garnero <nicogarnero1@outlook.com>
2023-03-14 08:59:39 -0300

    :subiendo progreso

--- ta_te_ti.py
+++ ta_te_ti.py
@@ -22,4 +22,13 @@ turno = 0
 imprimir_tablero(tablero)
 while turno < 9:
     if jugador_1 == " ":
-                         
+        print("Nombre de jugador 1 (x)")
+        jugador_1 = input()
+        print("Nombre de jugador 2 (o)")
+        jugador_2 = input()
+    else:
+        if turno_1:
+            print(jugador_1 + ", elegí una posición")
+        else:
+            print(jugador_2 + ", elegí una posición")
+        jugada = int(input())
